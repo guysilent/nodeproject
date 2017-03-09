84782e5e3f370e8b08bae9b8a0f80e322e4bc802
guy <guy.silent@gmail.com>
2017-03-09 15:51:25 +0000

    Second commit

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,19 @@
-var guy = 'hello guy';
-console.log(guy);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+app.get('/', function(request, response){
+    response.send('Hello World!');
+});
+
+app.get('/routing', function(request, response){
+    response.send('Hello Routing');
+});
+
+app.listen(port, function(err){
+    console.log('The Server is Running On port: ' + port);
+})
